0e8cff7d9a500a382b47850cf722d3f899e49825
Manish9797 <ukirdemanish9797@gmail.com>
2019-08-17 17:12:35 +0530

    updated swagger

--- controllers/subTaskController.js
+++ controllers/subTaskController.js
@@ -1,21 +1,64 @@
 
-
-
 let service = require('../serviceses/subtaskServices')
-
-module.exports= class subTaskController{
+module.exports = class subTaskController {
     constructor(app) {
-        this.app= app
+        this.app = app
         this.routs()
     }
-    routs(){
-        this.app.get('/api/v1/users/:userid/tasks/:taskid/subtasks/',async (request,response)=>{
-              let taskid=request.params.taskid
-              let data= await service.getSubTasks(taskid) ;
-              console.log(data)
-              response.send(data)      
+    routs() {
+        this.app.get('/api/v1/users/:userid/tasks/:taskid/subtasks/', async (request, response) => {
+            let taskid = request.params.taskid
+            let data = await service.getSubTasks(taskid);
+            console.log(data)
+            response.send(data)
         })
-       
+        this.app.get('/api/v1/users/:userid/tasks/:taskid/subtasks/:subtaskid', async (request, response) => {
+            let taskid = request.params.taskid;
+            let subtaskid = request.params.subtaskid;
+            let data = await service.getSingleSubtaskService(taskid, subtaskid)
+            console.log(data)
+            response.send(data)
+        })
+        this.app.delete('/api/v1/users/:userid/tasks/:taskid/deletesubtasks/:subtaskid', async (request, response) => {
+            let subtaskid = request.params.subtaskid;
+            let taskid = request.params.taskid
+            service.deleteSubtaskServive(taskid, subtaskid).then(function (result) {
+                console.log(result)
+            })
 
+        })
+        this.app.post('/api/v1/users/:userid/tasks/:taskid/addsubtasks', async (request, response) => {
+            let taskid = request.params.taskid
+            let tasktitle = request.body.tasktitle
+            let taskdiscription = request.body.taskdiscription
+            let priority = request.body.priority
+            let taskDate = request.body.taskDate
+            let subtaskobj = {
+                tasktitle: tasktitle,
+                taskdiscription: taskdiscription,
+                priority: priority,
+                taskDate: taskDate
+            }
+            service.addSubtaskService(taskid, subtaskobj).then(function (result) {
+                    console.log('added')
+                })
+                .catch(function (err) {
+                    console.log('some error occuredd')
+                })
+        })
+
+        this.app.put('/api/v1/users/:userid/tasks/:taskid/editsubtasks/:subtaskid', (request, response) => {
+            let taskid = request.params.taskid
+            let subtaskid = request.params.subtaskid
+           
+            let tasktitle = request.body.tasktitle
+            let taskdiscription = request.body.taskdiscription
+            let priority = request.body.priority
+            let taskDate = request.body.taskDate
+            service.updateSubtask(tasktitle, taskdiscription, priority, taskDate, taskid, subtaskid).then(function (result) {
+                console.log(result)
+            })
+            response.send('updated')
+        })
     }
 }
--- serviceses/subtaskServices.js
+++ serviceses/subtaskServices.js
@@ -1,144 +1,151 @@
-const mongoose = require('mongoose')
-const mongo = require('mongodb');
-const taskservice = require('./taskServices')
-const url = "mongodb+srv://MANISH:MANISH%409797@cluster0-qdzy9.mongodb.net/task-manager"
-mongoose.connect(url, {
-    useNewUrlParser: true
-})
-let userSchema = new mongoose.Schema({
-    name: {
-      fname: String,
-      lname: String
-    },
-    username: String,
-    password: String,
-    mobileno: String,
-    registerdate: Date,
-    userStatus: {
-      type: Boolean,
-      default: true
-    },
-    Task: [{
-      priority: String,
-      tasktitle: String,
-      taskdiscription: String,
-      taskcreated: Date,
-      taskDate: Date,
-      //  status: Boolean,
-  
-      subtask: [{
-        priority: String,
-        tasktitle: String,
-        taskdiscription: String,
-        taskcreated: Date,
-        taskDate: Date,
-        //    status: Boolean
-      }]
-    }]
-  
-  })
-  
-let userModel = mongoose.model('Users1', userSchema)
-async function getSubtasks(id){
-    let data = await taskservice.getSingleTask(id)
- //   console.log(data[0].Task.subtask)
-    return data[0].Task.subtask
-}
-
-async function addsubTask(taskid,subtaskobj){
-    let subtasks = await getSubtasks(taskid)
-    subtasks.push(subtaskobj)
-    updateTaskWithSubtask(taskid,subtasks)
-}
-async function deleteSubtask(taskid,subtaskid){
-    let subtasks = await getSubtasks(taskid)
-    for(index=0;index<subtasks.length;index++){
-        if(subtasks[index]._id==subtaskid){
-            subtasks.splice(index,1)
-            break;
-        }
+/** 
+ * @swagger
+ * tags:
+ *    - name: Task
+ *      description: everything about Task
+ * /api/v1/users/{userid}/tasks/{taskid}/subtasks:
+ *   get:
+ *     tags: 
+ *       - SubTask
+ *     summary: return the tasks of user by id
+ *     produces:
+ *       - application/json 
+ *     parameters:
+ *       - in: path
+ *         name: userid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *       - in: path
+ *         name: taskid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ * 
+ *     responses:
+ *        default:
+ *          description: Default error sample response
+ * 
+ * /api/v1/users/{userid}/tasks/{taskid}/subtasks/{subtasksid}/:
+ *   get: 
+ *     tags: 
+ *       - SubTask
+ *     summary: Add the Task into user of id above
+ *     parameters:
+ *       - in: path
+ *         name: userid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *       - in: path
+ *         name: subtasksid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *       - in: path
+ *         name: taskid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *     responses:
+ *        default:
+ *          description: Default error sample response
+ *
+ * /api/v1/users/{userid}/tasks/{taskid}/deletesubtasks/{subtasksid}:
+ *   delete:
+ *     tags: 
+ *       - SubTask 
+ *     summary: delete the task
+ *     produces:
+ *       - application/json 
+ *     parameters:
+ *       - in: path
+ *         name: userid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *       - in: path
+ *         name: subtasksid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get
+ *       - in: path
+ *         name: taskid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: string ID of the contact to get 
+ *     responses:
+ *        default:
+ *          description: Default error sample response
+*/
+let service = require('../serviceses/subtaskServices')
+module.exports = class subTaskController {
+    constructor(app) {
+        this.app = app
+        this.routs()
     }
-    updateTaskWithSubtask(taskid,subtasks)
-}
+    routs() {
+        this.app.get('/api/v1/users/:userid/tasks/:taskid/subtasks/', async (request, response) => {
+            let taskid = request.params.taskid
+            let data = await service.getSubTasks(taskid);
+            console.log(data)
+            response.send(data)
+        })
+        this.app.get('/api/v1/users/:userid/tasks/:taskid/subtasks/:subtaskid', async (request, response) => {
+            let taskid = request.params.taskid;
+            let subtaskid = request.params.subtaskid;
+            let data = await service.getSingleSubtaskService(taskid, subtaskid)
+            console.log(data)
+            response.send(data)
+        })
+        this.app.delete('/api/v1/users/:userid/tasks/:taskid/deletesubtasks/:subtaskid', async (request, response) => {
+            let subtaskid = request.params.subtaskid;
+            let taskid = request.params.taskid
+            service.deleteSubtaskServive(taskid, subtaskid).then(function (result) {
+                console.log(result)
+            })
 
-async function getSingleSubTask(taskid,subtaskid){
-  let subtasks = await getSubtasks(taskid)
-  let data ;
-  for(index=0;index<subtasks.length;index++){
-      if(subtasks[index]._id==subtaskid){
-          data = subtasks[index]
-          break;
-      }
-  }
-  //console.log(data)
-     return data
-}
-//getSingleSubTask('5d53a34af2d91b2654091007','5d53a34af2d91b2654091008')
+        })
+        this.app.post('/api/v1/users/:userid/tasks/:taskid/addsubtasks', async (request, response) => {
+            let taskid = request.params.taskid
+            let tasktitle = request.body.tasktitle
+            let taskdiscription = request.body.taskdiscription
+            let priority = request.body.priority
+            let taskDate = request.body.taskDate
+            let subtaskobj = {
+                tasktitle: tasktitle,
+                taskdiscription: taskdiscription,
+                priority: priority,
+                taskDate: taskDate
+            }
+            service.addSubtaskService(taskid, subtaskobj).then(function (result) {
+                    console.log('added')
+                })
+                .catch(function (err) {
+                    console.log('some error occuredd')
+                })
+        })
 
-//getSubtasks('5d53a34af2d91b2654091007')
-async function editSubtask(tasktitle,taskdiscription,priority,taskDate,taskid,subtaskid){
-
-    let subtasks = await getSubtasks(taskid)
-    let data = {
-        _id:subtaskid,
-        tasktitle:tasktitle,
-        taskdiscription:taskdiscription,
-        priority:priority,
-        taskDate:taskDate
+        this.app.put('/api/v1/users/:userid/tasks/:taskid/editsubtasks/:subtaskid', (request, response) => {
+            let taskid = request.params.taskid
+            let subtaskid = request.params.subtaskid
+           
+            let tasktitle = request.body.tasktitle
+            let taskdiscription = request.body.taskdiscription
+            let priority = request.body.priority
+            let taskDate = request.body.taskDate
+            service.updateSubtask(tasktitle, taskdiscription, priority, taskDate, taskid, subtaskid).then(function (result) {
+                console.log(result)
+            })
+            response.send('updated')
+        })
     }
-
-    for(index=0;index<subtasks.length;index++){
-        if(subtasks[index]._id==subtaskid){
-          subtasks[index]= data;
-            break;
-        }
-    }
-    updateTaskWithSubtask(taskid,subtasks)
 }
-//editSubtask('change123','change123','LOW','1997-09-06T18:30:00.000+00:00','5d53a34af2d91b2654091007','5d53a34af2d91b2654091008')
-
-async function updateTaskWithSubtask(id,subtasks){
-    const o_id = new mongo.ObjectID(id);
-
-    let data;
-    await userModel.updateOne({
-      Task: {
-        $elemMatch: {
-          _id: id
-        }
-      }
-    }, {
-      $set: {
-        'Task.$.subtask':subtasks,
-        
-      }
-    }, (err, res) => {
-      if (err) throw err;
-      data = res;
-
-    })
-    return data;
-}
-module.exports = {
-    getSubTasks: async function (id) {
-        let data = await getSubtasks(id)
-        return data;
-    },
-    getSingleSubtaskService: async function(taskid,subtaskid){
-        let data = await getSingleSubTask(taskid,subtaskid)
-        return data
-    },
-    updateSubtask: async function (tasktitle,taskdiscription,priority,taskDate,taskid,subtaskid){
-        editSubtask(tasktitle,taskdiscription,priority,taskDate,taskid,subtaskid)
-    },
-    deleteSubtaskServive: async  function(taskid,subtaskid){
-        deleteSubtask(taskid,subtaskid)
-    },
-    addSubtaskService: async function (taskid,subtaskobj){
-        addsubTask(taskid,subtaskobj)
-    }
-
-}
-
-
- 
